be19650e15166913b929e03886398bdc9774e075
Arthur <arthurmielemalveste@gmail.com>
2024-03-25 15:04:19 -0300

    fazer menu inicial

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from lib.funcao import funcao_sala, funcao_equipamento
 
-funcao_sala()
-# funcao_equipamento()
+funcao = int(input("""\nQual o setor que você quer entrar ?
+      1 - Sala
+      2 - Equipamento
+      3 - Outros\n:"""))
+
+if funcao == 1:
+    funcao_sala()
+elif funcao == 2:
+    funcao_equipamento()
+
